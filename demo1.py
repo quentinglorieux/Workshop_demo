a18560c9f78f9f5d515cb913e21bbd406da70c0a
Quentin Glorieux <quentin.glorieux@gmail.com>
2024-11-04 06:44:24 +0100

    first commit

---
+++ demo1.py
@@ -0,0 +1,13 @@
+def sum_three_numbers(a, b, c):
+    """
+    Calculate the sum of three numbers.
+
+    Args:
+        a (int or float): The first number.
+        b (int or float): The second number.
+        c (int or float): The third number.
+
+    Returns:
+        int or float: The sum of the three numbers.
+    """
+    return a + b + c
